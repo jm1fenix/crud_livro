aa4a35068dc0d0e109c3835ec46ba252aad4610f
jm1fenix <franciscogasperini@gmail.com>
2024-04-08 09:14:27 -0300

    meumanonegoborges

---
+++ src/App.jsx
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css'
+
+function App() {
+  const [livros, setLivros] = useState([]);
+  const [novoLivro, setNovoLivro] = useState({
+    isbn: '',
+    titulo: '',
+    genero: '',
+    editora: '',
+    autor: '',
+  });
+
+  const fetchLivros = async () => {
+    try {
+      const response = await axios.get('http://localhost:8090/livros');
+      setLivros(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar livros:', error);
+    }
+  };
+  
+  useEffect(() => {
+    fetchLivros();
+  }, []);
+  
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNovoLivro((prevLivro) => ({
+      ...prevLivro,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post('http://localhost:8090/livros', novoLivro);
+      fetchLivros();
+      setNovoLivro({
+        isbn: '',
+        titulo: '',
+        genero: '',
+        editora: '',
+        autor: '',
+      });
+    } catch (error) {
+      console.error('Erro ao criar livro:', error);
+    }
+  };
+    
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8090/livros/${id}`);
+      fetchLivros();
+    } catch (error) {
+      console.error('Erro ao excluir livro:', error);
+    }
+  };
+  
+  const handleUpdate = async (id, livroAtualizado) => {
+    try {
+      await axios.put(`http://localhost:8090/livros/${id}`, livroAtualizado);
+      fetchLivros();
+    } catch (error) {
+      console.error('Erro ao atualizar livro:', error);
+    }
+  };
+  
+  return (
+    <>
+  <div>
+    {/* Cabeçalho */}
+    <h1>Gerenciamento de Livros</h1>
+
+    {/* Formulário de adição de livro */}
+    <form onSubmit={handleSubmit}>
+      {/* Campo para a isbn */}
+      <input
+        type="text"
+        name="isbn"
+        placeholder="isbn"
+        value={novoLivro.isbn}
+        onChange={handleInputChange}
+      />
+      {/* Campo para a titulo */}
+      <input
+        type="text"
+        name="titulo"
+        placeholder="Título"
+        value={novoLivro.titulo}
+        onChange={handleInputChange}
+      />
+       {/* Campo para a genero */}
+       <input
+        type="text"
+        name="genero"
+        placeholder="Gênero"
+        value={novoLivro.genero}
+        onChange={handleInputChange}
+      />
+      {/* Campo para o editora */}
+      <input
+        type="text"
+        name="editora"
+        placeholder="Editora"
+        value={novoLivro.editora}
+        onChange={handleInputChange}
+      />
+      {/* Campo para o autor */}
+      <input
+        type="number"
+        name="autor"
+        placeholder="Autor"
+        value={novoLivro.autor}
+        onChange={handleInputChange}
+      />
+      {/* Botão de envio do formulário */}
+      <button type="submit">Adicionar Livro</button>
+    </form>
+
+    {/* Lista de livros */}
+    <ul>
+      {/* Mapeamento dos livros */}
+      {livros.map((livro) => (
+        <li key={livro.id}>
+          {/* Exibição dos detalhes do livro */}
+          {livro.isbn} - {livro.titulo} {livro.genero} {livro.editora} ({livro.autor})
+          
+          {/* Botão de exclusão */}
+          <button onClick={() => handleDelete(livro.id)}>Excluir</button>
+          
+          {/* Botão de atualização */}
+          <button
+            onClick={() =>
+              handleUpdate(livro.id, {
+                ...livro,
+                editora: 'Novo editora Atualizado', // Exemplo de atualização
+              })
+            }
+          >
+            Atualizar
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+
+    </>
+  )
+}
+
+export default App
